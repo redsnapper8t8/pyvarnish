d6b5deb9ea7e18e648765948031ea150e00e40ca
John G. Moylan <john@8t8.eu>
2011-12-02 22:45:58 +0000

    Added License

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 __author__ = 'John Moylan'
 
-VARNISH_SERVERS = ('sqc.rte.ie', "sqf.rte.ie",)
-SSH_CONFIG = "/home/john/.ssh/config"
+VARNISH_SERVERS = ('server1', "server2",)
+SSH_CONFIG = "/home/username/.ssh/config"
 DEBUG = True
 CARBON_SERVER = 'localhost'
 CARBON_PORT = 2003
