d91cf2254fbbc6126d295ac7b0f5119a617820ae
John G. Moylan <john@8t8.eu>
2012-07-14 19:48:29 +0100

    Add some tests

--- pyvarnish/tests.py
+++ pyvarnish/tests.py
@@ -1,1 +1,23 @@
 __author__ = 'john'
+
+import unittest
+
+from pyvarnish.parse_stats import VarnishGather, msg
+
+
+class TestVarnishGather(unittest.TestCase):
+    """"""
+    def test_parse_xml(self):
+        """Constructor"""
+        server = "mytestserver"
+        vg = VarnishGather(server)
+        vg.parse_xml("test_data.xml")
+
+        message = msg(vg.lines)
+
+        self.assertEqual(message.strip(), open("test_message.txt").read())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
